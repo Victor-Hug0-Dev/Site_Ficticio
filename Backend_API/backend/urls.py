38ab16278445c7f7d38098a46e150db26c9e3fa2
Victor-Hug0-Dev <victor.hugodev@outlook.com>
2025-05-14 10:06:12 -0400

    adding urls to backend api

--- Backend_API/backend/urls.py
+++ Backend_API/backend/urls.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 urlpatterns = [   
    path('admin/',admin.site.urls),
    path('accounts/', include('allauth.urls')),
-   path('post/', include('posts.api.urls')),
-   path('user/', include('user.api.urls')),
+   path('api/', include('backend.api.urls')),
 ]
 

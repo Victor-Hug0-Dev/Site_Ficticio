c9aecf95cfa6d46bb98be6798cbef0b7cce52353
Victor-Hug0-Dev <victor.hugodev@outlook.com>
2025-05-15 09:25:58 -0400

    separate user router

--- Backend_API/user/api/serializers.py
+++ Backend_API/user/api/serializers.py
@@ -1,14 +1,25 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework.validators import UniqueValidator
-
 from .models import User
 
+
+#Serializer geral denerico
 class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'created', 'user_name', 'email', 'password')
+        
+class UserListSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'created', 'user_name', 'email')
+
+
+class UserDetailSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'created', 'user_name', 'email')
         
-
 class UserRegisterSerializer(ModelSerializer):
     class Meta:
         model = User
@@ -23,3 +34,11 @@ class UserRegisterSerializer(ModelSerializer):
                     ]
                 }
             }
+
+class UserDeleteSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id')
+       
+        
+    
--- Backend_API/user/api/views.py
+++ Backend_API/user/api/views.py
@@ -2,12 +2,20 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import generics
 from .models import User
-from .serializers import UserSerializer
+from user.api import serializers
 
-class UserViewSet(generics.ListAPIView):
+class UserListViewSet(generics.ListAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = serializers.UserListSerializer
+
+class UserDetailViewSet(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserDetailSerializer
 
 class UserRegisterViewSet(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = serializers.UserRegisterSerializer
+
+class UserDeleteViewSet(generics.DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserDeleteSerializer

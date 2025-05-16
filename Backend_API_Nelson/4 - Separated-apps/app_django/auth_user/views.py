97683807cb81ba82a84a0e36cfb5fa289bd33f62
Nelson-esilva <nesi.ele24@uea.edu.br>
2025-05-16 10:56:51 -0400

    adicionado o profile

--- Backend_API_Nelson/4 - Separated-apps/app_django/auth_user/views.py
+++ Backend_API_Nelson/4 - Separated-apps/app_django/auth_user/views.py
@@ -10,12 +10,36 @@ class CustomLoginView(ObtainAuthToken):
 
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
-        token = Token.objects.get(key=response.data['token'])
+
+        #Verifica se o usuário já está logado)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+
+        token_key = response.data['token']
+       
+        #token = Token.objects.get(key=response.data['token'])
+        token, created = Token.objects.get_or_create(user=user)
+        #(key=token_key)
+        user = token.user
+
+        #/////////////////// VERIFICAÇÕES ///////////////////////
+
+
+        # Verifica se o usuário está ativo
+        if not user.is_active:
+            return Response({'error': 'Usuário inativo.'}, status=403)
+        # Verifica se o usuário está autenticado
+        if not user.is_authenticated:
+            return Response({'error': 'Usuário não autenticado.'}, status=403)
+        
         return Response({
             'token': token.key,
             'user_id': token.user_id,
             'email': token.user.email,
-            'username': token.user.username
+            'username': token.user.username,
+            'created_at': token.created,
+            'created': created,
         })
 
 class LogoutView(APIView):
--- Backend_API_Nelson/4 - Separated-apps/app_django/user/views.py
+++ Backend_API_Nelson/4 - Separated-apps/app_django/user/views.py
@@ -2,7 +2,10 @@ from rest_framework import generics, permissions
 from .models import User
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import viewsets
+
 
 class RegisterUserView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -13,3 +16,13 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            "email": user.email,
+            "username": user.username
+        })

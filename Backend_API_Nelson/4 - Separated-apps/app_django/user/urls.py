1f613728d510a043f83c2ac870b014d9cde2a785
Nelson-esilva <nesi.ele24@uea.edu.br>
2025-05-16 10:56:51 -0400

    adicionado o profile

--- Backend_API_Nelson/4 - Separated-apps/app_django/user/urls.py
+++ Backend_API_Nelson/4 - Separated-apps/app_django/user/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import RegisterUserView
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet
+from .views import ProfileView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
+    path('profile/', ProfileView.as_view(), name='user-profile'),
 ] + router.urls
 

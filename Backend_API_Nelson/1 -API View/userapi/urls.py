2b454f89fe03d1eccbf09d84cc639bd0de0486bc
Nelson-esilva <nesi.ele24@uea.edu.br>
2025-05-22 14:11:40 -0400

    adicionando allauth

--- Backend_API_Nelson/1 -API View/userapi/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserListView, ProfileListView
-
-
-urlpatterns = [
-    path('users/', UserListView.as_view(), name='user-list'),
-    path('profiles/', ProfileListView.as_view(), name='profile-list'),
-]

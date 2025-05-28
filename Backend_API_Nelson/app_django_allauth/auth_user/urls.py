c8314c31821ac9c486dd6d2f17d5cbacdb102f34
Nelson-esilva <nesi.ele24@uea.edu.br>
2025-05-28 10:03:21 -0400

    refactory

--- Backend_API_Nelson/app_django_allauth/auth_user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import GoogleLogin
-
-urlpatterns = [
-    #path('accounts/google/login/callback/', GoogleCallbackView.as_view(), name='google_callback'),
-    path("api/auth/google/", GoogleLogin.as_view(), name='google_login'),
-]
